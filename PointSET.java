970176ace59e9093ec2a9453430a1aa2b8955569
Dubya93 <64231486+Dubya93@users.noreply.github.com>
2021-10-20 14:15:24 -0500

    Add files via upload

---
+++ PointSET.java
@@ -0,0 +1,70 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+public class PointSET 
+{
+	private SET<Point2D> set;
+
+    // construct an empty set of points
+    public PointSET() {
+        set = new SET<Point2D>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return set.size();
+    }
+
+    // add the point p to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        set.add(p);
+    }
+
+    // does the set contain the point p?
+    public boolean contains(Point2D p) {
+        return set.contains(p);
+    }
+
+    // draw all of the points to standard draw
+    public void draw() {
+        for (Point2D p : set) {
+            p.draw();
+        }
+    }
+
+    // all points in the set that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        Queue<Point2D> q = new Queue<Point2D>();
+
+        for (Point2D p : set) {
+            if (rect.contains(p)) {
+                q.enqueue(p);
+            }
+        }
+
+        return q;
+    }
+
+    // a nearest neighbor in the set to p; null if set is empty
+    public Point2D nearest(Point2D p) {
+        Point2D nearestNeighbor = null;
+        double nearestDistance = Double.MAX_VALUE;
+
+        for (Point2D neighbor : set) {
+            double distance = p.distanceTo(neighbor);
+            if (distance < nearestDistance) {
+                nearestNeighbor = neighbor;
+                nearestDistance = distance;
+            }
+        }
+
+        return nearestNeighbor;
+    }
+}
